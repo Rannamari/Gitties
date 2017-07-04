a1c475c7bce8dc5bfceea490c2220b59c87a75ed
gabor-szeles <szelesgbr@gmail.com>
2017-07-04 09:54:45 +0200

    implemented a very funny joke LOL

--- gittie.py
+++ gittie.py
@@ -1,2 +1,9 @@
 def introduce():
     print("Hello, I'm Gittie")
+
+
+def joke():
+    print("How do you know a politician's lying?")
+    print("Their lips are moving")
+    print("<Insert unending laughter>")
+    
